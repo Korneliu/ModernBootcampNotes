b794a9e5006dbe8b16650876e01d2ec5e37b0326
Korneliu <olbiton@gmail.com>
2019-05-14 22:14:28 -0700

    71 The Edit Note Page Part II

--- notes-app.js
+++ notes-app.js
@@ -1,26 +1,31 @@
-// DOM-Document Object Model
 const notes = getSavedNotes()
 
 const filters = {
-  searchText: ''
+    searchText: ''
 }
-saveNotes(notes)
+
 renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
-  notes.push({
-    title: '',
-    body: ''
-  })
-  
-  renderNotes(notes, filters)
+    const id = uuidv4()
+
+    notes.push({
+        id: id,
+        title: '',
+        body: ''
+    })
+    saveNotes(notes)
+    location.assign(`/edit.html#${id}`)
 })
 
- document.querySelector('#search-text').addEventListener('input', function(e) {
-  filters.searchText = e.target.value
-  renderNotes(notes, filters)
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
 
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
+})
 
 
 
