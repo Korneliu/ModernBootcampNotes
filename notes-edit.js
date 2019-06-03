beffeadd4bd9e9104de6095c0ad58eff9fc0ce9a
Korneliu <olbiton@gmail.com>
2019-06-02 22:49:07 -0700

    added conditional Operator, and Truthy and Falsy Values

--- notes-edit.js
+++ notes-edit.js
@@ -6,7 +6,7 @@ const noteId = location.hash.substring(1)
 let notes = getSavedNotes()
 let note = notes.find((note) => note.id === noteId)
 
-if (note === undefined) {
+if (!note) {
     location.assign('/index.html')
 }
 
@@ -38,7 +38,8 @@ window.addEventListener('storage', (e) => {
     if (e.key === 'notes') {
         notes = JSON.parse(e.newValue)
         note = notes.find((note) => note.id === noteId)
-        if (note === undefined) {
+        
+        if (!note) {
             location.assign('/index.html')
         }
 
--- notes-functions.js
+++ notes-functions.js
@@ -3,13 +3,12 @@
 const getSavedNotes = () => {
     const notesJSON = localStorage.getItem('notes')
 
-    if (notesJSON !== null) {
-        return JSON.parse(notesJSON)
-    } else {
+    try {
+        return notesJSON ? JSON.parse(notesJSON) : []
+    } catch(e) {
         return []
-    }
+    } 
 }
-
 // Save the notes to localStorage
 const saveNotes = (notes) => {
     localStorage.setItem('notes', JSON.stringify(notes))
